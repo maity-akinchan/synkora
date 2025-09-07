5e0e10d3c08867ee3bd3f858b8f5d19ec5a8bf77
Akincan Maity <akinchan0023.becse24@chitkara.edu.in>
2025-09-07 19:47:07 +0000

    Major Improvments

--- src/components/user/SecurityCard.tsx
+++ src/components/user/SecurityCard.tsx
@@ -2,12 +2,12 @@
 
 export default function SecurityCard() {
   return (
-    <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col">
-      <h3 className="text-lg font-semibold text-gray-800 mb-2">Security</h3>
-      <button className="bg-red-500 text-white px-4 py-2 rounded-lg text-sm hover:bg-red-600 transition mb-2">
+    <div className="bg-[var(--background)] rounded-2xl shadow-lg p-6 flex flex-col">
+      <h3 className="text-lg font-semibold text-[var(--secondary)] mb-2">Security</h3>
+      <button className="bg-red-500 text-[var(--foreground)] px-4 py-2 rounded-lg text-sm hover:bg-red-600 transition mb-2">
         Change Password
       </button>
-      <button className="bg-gray-800 text-white px-4 py-2 rounded-lg text-sm hover:bg-gray-900 transition">
+      <button className="bg-gray-800 text-[var(--foreground-alt)] px-4 py-2 rounded-lg text-sm hover:bg-gray-900 transition">
         Enable 2FA
       </button>
     </div>
